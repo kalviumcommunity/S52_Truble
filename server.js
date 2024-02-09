69e91b95fe94160c02bf07017934e9d6b39c8960
Jaiabhisshek8 <jaiabhissheks@gmail.com>
2024-02-09 16:30:59 +0530

    Connected to DB

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const mongoose=require("mongoose")
+require('dotenv').config()
 const app = express()
-app.get("/ping",(req, res)=>{
-    res.send("pong")
+const mongouri=process.env.mongouri
+mongoose.connect(mongouri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+const db=mongoose.connection
+db.on('error', console.error.bind(console, "Mongo DB connection error"))
+db.once('open', ()=>{
+    console.log("Connected to mongoDB through mongoose")
+})
+app.get("/",(req, res)=>{
+    if(db.readyState===1){
+        res.send("Connected to mongoDB through mongoose")
+    }else{
+        res.send("failed to connect to mangodb")
+    }
+})
+
 app.listen(3000, ()=>{
     console.log("Server is running at local host : 3000")
 })
